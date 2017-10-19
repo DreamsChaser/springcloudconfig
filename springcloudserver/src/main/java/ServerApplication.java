62c193f0533c42a0990ff3679c881f501853d052
leon <18661230709@163.com>
2017-10-19 16:01:26 +0800

    #spring-cloud-config

---
+++ springcloudserver/src/main/java/ServerApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author leon
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class,args);
+    }
+}
